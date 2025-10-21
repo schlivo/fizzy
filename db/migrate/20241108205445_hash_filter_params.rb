25598e75bd8a50c0795080a9ce72d2c617e52e68
Mike Dalessio <mike@37signals.com>
2025-10-20 17:44:17 -0700

    Fix old migration that doesn't work with Rails edge

--- db/migrate/20241108205445_hash_filter_params.rb
+++ db/migrate/20241108205445_hash_filter_params.rb
@@ -1,7 +1,7 @@
 class HashFilterParams < ActiveRecord::Migration[8.0]
   def change
-    change_column :filters, :params, :string, null: false
     change_column_default :filters, :params, from: {}, to: nil
+    change_column :filters, :params, :string, null: false
     rename_column :filters, :params, :params_digest
   end
 end
